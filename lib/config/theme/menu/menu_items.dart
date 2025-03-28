e72fdcaef3d10cfd658ecbd6584207e91549b537
Douglas Mena <douglasmena2015pc@gmail.com>
2025-03-27 22:30:06 -0500

    progress indicator finished

--- lib/config/theme/menu/menu_items.dart
+++ lib/config/theme/menu/menu_items.dart
@@ -24,11 +24,17 @@ const appMenuItems = <MenuItem>[
     icon: Icons.smart_button_outlined
   ),
   
-    MenuItem(
+  MenuItem(
     title: 'Tarjetas',
     subtitle: 'Un contenedor estilizado',
     link: '/cards',
     icon: Icons.credit_card_outlined
+  ),
+  MenuItem(
+    title: 'ProgressIndicators',
+    subtitle: 'Generales y controlados',
+    link: '/progress',
+    icon: Icons.refresh_rounded
   ),
 
 ];
